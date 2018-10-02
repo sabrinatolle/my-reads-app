6a3ba53e4099183ac3ffb12ee3c98ea3a3c3a4f7
Sabrina Tolle <sabrinatolle@outlook.com>
2018-10-02 19:12:22 -0400

    move search content to SearchPage

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -3,7 +3,20 @@ import React from 'react';
 class SearchPage extends React.Component {
     render() {
         return(
-            <p>search</p>
+           <div className="search-books">
+                  <div className="search-books-bar">
+                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <div className="search-books-input-wrapper">
+                     
+                      <input type="text" placeholder="Search by title or author"/>
+      
+                    </div>
+                  </div>
+                  <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                  </div>
+                </div>
+           
         );
     }
 }
