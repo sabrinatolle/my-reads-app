4081ca962b833c04ee22b332de2c7a5f45d9d124
Sabrina Tolle <sabrinatolle@outlook.com>
2018-10-02 18:58:02 -0400

    import SearchPage

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css'
 
 import { Route } from 'react-router-dom'
 import MainPage from './components/pages/MainPage';
+import SearchPage from './components/pages/SearchPage';
 
 class BooksApp extends React.Component {
   render() {
     return {
       <div> 
         <Route exact path='/' component= { MainPage } />
+        <Route exact path='/' component= { SearchPage } />
       </div>
      };
   
