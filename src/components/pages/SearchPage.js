d9405c5f7f7e451216706fbd95919c52921d436d
Sabrina Tolle <sabrinatolle@outlook.com>
2018-10-02 19:17:32 -0400

    link SearchPage to the root

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 class SearchPage extends React.Component {
     render() {
-        return(
+        return (
            <div className="search-books">
                   <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className="close-search"to='/'>Close</Link>
                     <div className="search-books-input-wrapper">
                      
                       <input type="text" placeholder="Search by title or author"/>
