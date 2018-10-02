18880514373fa19caa55f8f5394f448ceba2e684
Sabrina Tolle <sabrinatolle@outlook.com>
2018-10-02 18:59:21 -0400

    create a class

---
+++ src/components/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchPage extends React.Component {
+    render() {
+        return(
+            <p>search</p>
+        );
+    }
+}
+
+export default SearchPage;
